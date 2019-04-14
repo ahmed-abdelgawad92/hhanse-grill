28a6c2850b8cd2acf7b7e01b9a98acd030b31b3a
Ahmed Abdelgawad <ahmed.elkayaty92@gmail.com>
2019-04-14 21:28:07 +0200

    Add the ability to edit the the daily meals

--- src/components/admin/admin-panel/menu/menu-item/MenuItem.vue
+++ src/components/admin/admin-panel/menu/menu-item/MenuItem.vue
@@ -10,21 +10,53 @@
           </button>
         </div>
         <div class="toast-body">
-          Das Angebot ist erfolgreich gelöscht !
+          {{success}}
         </div>
       </div>
     </div>
+    <div class="alert alert-danger" v-if="error">{{error}}</div>
     <div class="card">
       <div class="card-body">
         <div class="card-title">
-          {{item.row_order}} - {{item.meal.name | capitalize}}
-          <span class="subtitle">{{item.price}} &euro;</span>
+          <template v-if="edit">
+            <div class="row">
+              <div class="col-2">
+                <select v-model="number" class="form-control" :class="{'is-invalid': $v.number.$error}" @blur="$v.number.$touch()">
+                  <option value="1">I</option>
+                  <option value="2">II</option>
+                  <option value="3">III</option>
+                  <option value="4">IV</option>
+                </select>
+              </div>
+              <div class="col-8">
+                <input type="text" v-model.trim="meal" class="form-control" @blur="$v.meal.$touch()" :class="{'is-invalid': $v.meal.$error}">
+              </div>
+              <div class="col-2">
+                <input type="text" v-model.trim="price" class="form-control" @blur="$v.price.$touch()" :class="{'is-invalid': $v.price.$error}">
+              </div>
+            </div>
+          </template>
+          <template v-else>
+            {{item.row_order}} - {{item.meal.name | capitalize}}
+            <span class="subtitle">{{item.price}} &euro;</span>
+          </template>
         </div>
-        <p class="card-text">
+        <div v-if="edit">
+          <input type="text" v-model.trim="ingredient" class="form-control my-2">
+        </div>
+        <p v-else class="card-text">
           {{getIngredient()}}
         </p>
+        <span class="text-danger" v-if="!$v.meal.required">Das Gericht kann nicht leer sein</span>
+        <span class="text-danger" v-if="!$v.number.required">Die Nummer kann nicht leer sein</span>
+        <span class="text-danger" v-if="!$v.number.integer">Die Nummer muss eine Zahl sein</span>
+        <span class="text-danger" v-if="!$v.price.required">Der Preis kann nicht leer sein</span>
+        <span class="text-danger" v-if="!$v.price.decimal">Der Preis muss eine Zahl sein</span>
         <template v-if="$router.currentRoute.path == '/year-calendar'">
-          <button class="btn btn-danger" @click="confirmDeletion">löschen</button>
+          <button v-if="!edit" class="btn btn-secondary" @click="editShow">bearbeiten</button>
+          <button v-else class="btn btn-primary" type="submit" @click.prevent="submitEdit">speichern</button>
+          <button v-if="edit" class="btn btn-danger" @click="editShow">abbrechen</button>
+          <button v-else class="btn btn-danger" @click="confirmDeletion">löschen</button>
           <div class="modal" tabindex="-1" role="dialog" ref="modal">
             <div class="modal-dialog" role="document">
               <div class="modal-content">
@@ -56,10 +88,17 @@
 
 <script>
   import axios from 'axios';
+  import { required, integer, decimal } from 'vuelidate/lib/validators';
   export default {
     data: () => {
       return {
-        success: false
+        meal: '',
+        ingredient: '',
+        number: '',
+        price: '',
+        success: '',
+        error: '',
+        edit: false
       }
     },
     props: ['item'],
@@ -76,9 +115,9 @@
         axios.delete('delete_menu_item/' + this.item.id).then(
           response => {
             if(response.data.success){
-              this.success = true;
+              this.success = 'Das Angebot ist erfolgreich gelöscht !';
               setTimeout(() => {
-                this.success = false;
+                this.success = '';
               }, 3000);
               this.$emit('getAllIngredientsAndMeals');
               let element = this.$refs['modal'];
@@ -88,11 +127,56 @@
           error => {console.log(error)}
         );
       },
+      submitEdit: function(){
+        this.$v.$touch();
+        if(!this.$v.$invalid){
+          const data = {
+            meal: this.meal,
+            ingredient: this.ingredient,
+            price: this.price,
+            order: this.number
+          };
+          axios.put('edit_menu/' + this.item.id, data).then(response => {
+            this.success = response.data.success;
+            this.edit = false;
+            setTimeout(() => {
+              this.success = '';
+            }, 3000);
+            this.$emit('getAllIngredientsAndMeals');
+          }).catch(err=>{this.error = err.response.data.error});
+        }
+      },
       activate: function(){
         this.$emit('activate',this.item.id)
       },
       deactivate: function() {
         this.$emit('deactivate',this.item.id)
+      },
+      editShow: function(){
+        this.edit = !this.edit;
+        this.meal= this.item.meal.name;
+        this.ingredient= this.getIngredient();
+        this.number= this.item.row_order;
+        this.price= this.item.price;
+      }
+    },
+    created: function(){
+      this.meal= this.item.meal.name;
+      this.ingredient= this.getIngredient();
+      this.number= this.item.row_order;
+      this.price= this.item.price;
+    },
+    validations: {
+      number:{
+        required,
+        integer
+      },
+      price: {
+        required,
+        decimal
+      },
+      meal: {
+        required
       }
     }
   }
@@ -102,5 +186,12 @@
 span.subtitle{
   display: inline-block;
   float: right;
+}
+span.text-danger{
+  display: block;
+  font-size: 14px;
+}
+span.text-danger:last-of-type{
+  margin-bottom: 5px;
 }
 </style>
