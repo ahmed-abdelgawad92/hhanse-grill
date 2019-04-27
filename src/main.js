d9e0c0a3cb7da9524bce49eb253e9e1320d53523
Ahmed Abdelgawad <ahmed.elkayaty92@gmail.com>
2019-04-27 21:30:49 +0200

    create client files

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import Vuelidate from 'vuelidate';
 import axios from 'axios';
 import JWT from './jwt';
 
-axios.defaults.baseURL = 'https://api.hhansegrill-halal.de/api/';
+// axios.defaults.baseURL = 'https://api.hhansegrill-halal.de/api/';
 // axios.defaults.baseURL = 'http://hanse.ahmedelkayaty92.de/api/';
-// axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.baseURL = 'http://localhost:8000/api/';
 
 axios.interceptors.request.use(config => {
   config.headers['Authorization'] = JWT.getTokenAuthorization();
@@ -39,9 +39,9 @@ Vue.filter('articleNum', value => {
   return num;
 });
 
-Vue.prototype.$domain = 'https://api.hhansegrill-halal.de/';
+// Vue.prototype.$domain = 'https://api.hhansegrill-halal.de/';
 // Vue.prototype.$domain = 'http://hanse.ahmedelkayaty92.de/';
-// Vue.prototype.$domain = 'http://localhost:8000/';
+Vue.prototype.$domain = 'http://localhost:8000/';
 
 Vue.use(Vuelidate);
 
